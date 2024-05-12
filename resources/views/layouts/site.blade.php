d196e9481223baf6800147f5830a9adb9d215a4f
Alexander <alwong@ya.ru>
2024-05-12 15:39:44 +0400

    update

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -17,6 +17,9 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
+        <p style="color:red; background:white; transform:rotate(-30deg) translateX(-50%); position:absolute; left:50%; top:30%; z-index:10; font-size:20px; padding: 5px 20px; box-shadow: 1px 1px 10px 3px rgba(0,0,0, 0.4);">
+            [ The website is on develop mode ]
+        </p>
         <div class="wrapper">
             @include('includes.site.aside')
             @include('includes.site.top-panel')
