ff3ac5583e4138d12aaf3367e15fed2c2beed315
Alexander <alwong@ya.ru>
2024-05-18 15:12:33 +0400

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,39 +23,34 @@
     </section>
 
 
-
-
-
-    <section class="section section-next">
-        <h2 class="home-title">Ulyanovsk</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-    </section>
-
-    <section class="section section-next-2">
-        <h2 class="home-title">Ulyanovsk</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-    </section>
-
-    <section class="section section-next-3">
-        <h2 class="home-title">Ulyanovsk</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-    </section>
-
-    <section class="section section-next-4">
-        <h2 class="home-title">Ulyanovsk</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta sit necessitatibus obcaecati pariatur, similique, corrupti possimus dignissimos aliquam a nisi sequi sapiente, id quisquam? Dignissimos dolores ea praesentium aut earum?</p>
-    </section>
+    <section class="section section-map">
+        <div class="container">
+            <div class="section__title">
+                <h2>Ulyanovsk</h2>
+            </div>
+            <div style="position:relative;overflow:hidden;">
+                <a 
+                    href="https://yandex.ru/maps/195/ulyanovsk/?utm_medium=mapframe&utm_source=maps" 
+                    style="color:#eee;font-size:12px;position:absolute;top:0px;"
+                >
+                    Ульяновск
+                </a>
+                <a 
+                    href="https://yandex.ru/maps/195/ulyanovsk/?ll=48.403131%2C54.314194&utm_medium=mapframe&utm_source=maps&z=14" 
+                    style="color:#eee;font-size:12px;position:absolute;top:14px;"
+                >
+                    Яндекс Карты — транспорт, навигация, поиск мест
+                </a>
+                <iframe 
+                    src="https://yandex.ru/map-widget/v1/?ll=48.403131%2C54.314194&z=14" 
+                    width="100%" 
+                    height="500" 
+                    frameborder="1" 
+                    allowfullscreen="true" 
+                    style="position:relative;"
+                ></iframe>
+            </div>           
+        </div>
+    </section>           
 
 </x-site-layout>
