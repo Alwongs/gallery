e68f171de00245905439db07b5dea1d80f235763
Alexander <alwong@ya.ru>
2024-05-28 14:11:02 +0400

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,14 +26,14 @@
     <section class="section section-map">
         <div class="container">
             <div class="section__title">
-                <h2>Ulyanovsk</h2>
+                <h2>{{ __("common.ulyanovsk") }}</h2>
             </div>
             <div style="position:relative;overflow:hidden;">
                 <a 
                     href="https://yandex.ru/maps/195/ulyanovsk/?utm_medium=mapframe&utm_source=maps" 
-                    style="color:#eee;font-size:12px;position:absolute;top:0px;text-decoration:none"
+                    style="color:#eee;font-size:12px;position:absolute;top:0px;"
                 >
-                    {{ __("common.ulyanovsk") }}
+                    Ульяновск
                 </a>
                 <a 
                     href="https://yandex.ru/maps/195/ulyanovsk/?ll=48.403131%2C54.314194&utm_medium=mapframe&utm_source=maps&z=14" 
